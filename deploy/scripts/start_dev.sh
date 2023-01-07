1334fb52596b53b65edda74d98447ad91d29fbb8
Kevin C-Dubois <kcdubois@outlook.com>
2023-01-07 15:42:41 -0500

    updated deployment files

--- deploy/scripts/start_dev.sh
+++ deploy/scripts/start_dev.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-
+echo "Building project using the django project name"
 docker compose \
     --project-directory . \
     -p django \
     -f ./deploy/docker/compose.yaml build \
 
 
+echo "Starting Docker Compose for development"
 docker compose \
     --project-directory . \
     -p django \
